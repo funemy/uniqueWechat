5b930bb33620c06afa50fed3e4eeda14c3af50f9
Li Yanze <liyanze@LideMacBook-Pro.local>
2015-09-01 21:14:31 +0800

    settings

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 from tornado.ioloop import IOLoop
 from tornado.web import Application, url
 from tornado.options import define, options
+import base64, uuid
 
 import tornado.options
 import handler
 
 define('port', default=8000, help='run on the given port', type=int)
 
+settings = {
+    "cookie_secret": "O6R7HH++SnKHj1eD9TK0QD4sKJbM/0lLvv394rMuokQ="
+}
 
 def make_app():
     return Application([
         url(r"/", handler.MainHandler),
         url(r"/applicant", handler.ApplyHandler),
         url(r"/advice", handler.AdviceHandler)
-    ])
+    ], **settings)
 
 if __name__ == "__main__":
     tornado.options.parse_command_line()
